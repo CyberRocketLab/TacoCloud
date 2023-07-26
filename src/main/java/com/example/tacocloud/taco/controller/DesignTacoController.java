3cfb87b3b514149a60461d1279707b502a46b4d2
Alexandr Curanov <curanov21@gmail.com>
2023-07-26 10:40:34 +0200

    Creating IngridientConverter

--- src/main/java/com/example/tacocloud/taco/controller/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/taco/controller/DesignTacoController.java
@@ -7,10 +7,7 @@ import com.example.tacocloud.taco.domain.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -66,14 +63,12 @@ public class DesignTacoController {
                 .collect(Collectors.toList());
     }
 
-
-
-
-
-
-
-
-
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
+    }
 
 
 
